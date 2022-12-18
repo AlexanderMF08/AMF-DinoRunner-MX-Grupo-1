278e35bed7e2a10298f993038577676c1ff5540e
AlexanderMF08 <Alexander.Moruno@fundacion-jala.org>
2022-12-18 18:54:50 -0400

    Class 4: Powerups - Shield

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -26,7 +26,7 @@ class ObstacleManager:
               
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
-            if game.player.dino_rect.colliderect(obstacle.rect):
+            if game.player.dino_rect.colliderect(obstacle.rect) and game.player.shield == False:
                 game.player_heart_manager.reduce_heart()
 
                 if game.player_heart_manager.heart_count > 0:
